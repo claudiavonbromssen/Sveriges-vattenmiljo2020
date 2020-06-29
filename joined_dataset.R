bb9ec38b6f81637aee23bd915281789237fcce33
claudiavonbromssen <46597188+claudiavonbromssen@users.noreply.github.com>
2020-06-29 14:08:54 +0200

    Add files via upload

---
+++ joined_dataset.R
@@ -0,0 +1,121 @@
+if (exists("joined_dataset") == FALSE) {
+  tibble(dir="Indata2019/Sjöar/Växtplankton/") %>%
+    mutate(files = map(dir, ~list.files(here(.x)))) %>%
+    unnest(files) %>%
+    transmute(path = paste0(dir, files)) %>%
+    mutate(data = map(path, ~import_slu_mvm_data_excel(here(.x), numeric_var = 31, sheet = 3))) %>%
+    mutate(path = str_extract(basename(path), "^[^_]+(?=_)"),
+           Regionala = if_else(path %in% c("RMÖ","SRK"), 1, 0),
+           data = map2(data, Regionala, ~.x %>% mutate(Regionala = .y))) %>%
+    pull(data) %>%
+    reduce(full_join) %>%
+    select(1:30, Regionala, everything()) ->
+    indexberakningar
+  #Indexberakningar_data <- import_slu_mvm_data_excel(here("Indata2019/Sjöar/Växtplankton/Trend_stora_vplankton.xlsx"), numeric_var = 31, sheet = 3)
+  #IKEU_indexberakningar <- import_slu_mvm_data_excel(here("Indata2019/Sjöar/Växtplankton/IKEU_vplankton.xlsx"), numeric_var = 31, sheet = 3)
+  
+  #indexberakningar <- full_join(Indexberakningar_data, IKEU_indexberakningar)
+  
+  tibble(dir="Indata2019/Sjöar/Växtplankton/") %>%
+    mutate(files = map(dir, ~list.files(here(.x)))) %>%
+    unnest(files) %>%
+    transmute(path = paste0(dir, files)) %>%
+    mutate(data = map(path, ~import_slu_mvm_data_excel(here(.x), numeric_var = 29, sheet = 2))) %>%
+    mutate(path = str_extract(basename(path), "^[^_]+(?=_)"),
+           Regionala = if_else(path %in% c("RMÖ","SRK"), 1, 0),
+           data = map2(data, Regionala, ~.x %>% mutate(Regionala = .y))) %>%
+    pull(data) %>%
+    reduce(full_join) %>%
+    select(1:28, Regionala, everything()) %>%
+    mutate_at(30:42, Vectorize(function(x) {
+      if (is.na(x)) {
+        0
+      } else {
+        x
+      }
+    })) %>%
+    mutate(Biovolym2 = rowSums(.[, 30:(ncol(.))]) %>% round(3)) ->
+    vaxtplankton
+  
+  tibble(dir="Indata2019/Sjöar/Vattenkemi/") %>%
+    mutate(files = map(dir, ~list.files(here(.x)))) %>%
+    unnest(files) %>%
+    transmute(path = paste0(dir, files)) %>%
+    mutate(data = map(path, ~import_slu_mvm_data_excel(here(.x), numeric_var = 26, sheet = 2))) %>%
+    mutate(path = str_extract(basename(path), "^[^_]+(?=_)"),
+           Regionala = if_else(path %in% c("RMÖ","SRK"), 1, 0),
+           data = map2(data, Regionala, ~.x %>% mutate(Regionala = .y,
+                                                       Provkommentar = Provkommentar %>% as.character()))) %>%
+    pull(data) %>%
+    reduce(full_join) %>%
+    select(1:25, Regionala, everything()) ->
+    vattenkemi
+  
+  
+  #Vattenkemi_data <- import_slu_mvm_data_excel(here("Indata2019/Sjöar/Vattenkemi/trend_stora_kemi.xlsx"), 26)
+  #IKEU_Vattenkemi <- import_slu_mvm_data_excel(here("Indata2019/Sjöar/Vattenkemi/IKEU_kemi.xlsx"), 26)
+  #rename(`Kfyll (µg/l)` = `Kfyll (µg/l)...54`) %>%
+  # mutate(`Kfyll (µg/l)` = coalesce(`Kfyll (µg/l)`, `Kfyll (µg/l)...55`) %>% coalesce(`Kfyll (mg/m3)`)) %>%
+  #select(-`Kfyll (µg/l)...55`, -`Kfyll (mg/m3)`)
+  
+  #vattenkemi <- full_join(Vattenkemi_data, IKEU_Vattenkemi)
+  
+  indexberakningar %>% # join the files
+    full_join(vattenkemi) %>%
+    full_join(vaxtplankton) %>%
+    mutate( # `Kfyll (µg/l)` = coalesce(`Kfyll (mg/m3)`, `Kfyll (µg/l)`), # same unit, different names
+      `Min provdjup (m)` = coalesce(`Min provdjup (m)`, `Min Provdjup (m)`), # merge duplicate column
+      `Max provdjup (m)` = coalesce(`Max provdjup (m)`, `Max Provdjup (m)`),
+      `Biovolym (mm3/l)` = coalesce(`Biovolym (mm3/l)`, Biovolym2)
+    ) %>%
+    select( #-`Kfyll (mg/m3)`,
+      -`Min Provdjup (m)`, -`Max Provdjup (m)`, -Biovolym2
+    ) %>%
+    mutate(
+      `EU id` = case_when(
+        `EU id` == "SE655587-158869" ~ "SE655605-158820", # Stora Envättern   dessa tre från fisk eftersom de finns med i VISS
+        `EU id` == "SE656419-164404" ~ "SE656515-164330", # Stensjön
+        `EU id` == "SE656590-164240" ~ "SE656640-164224", # Längsjön"SE655275-153234"~ "NW655349-153274",  #Älgsjön
+        `EU id` == "SE656612-164132" ~ "SE656574-164098", # Årsjön
+        `EU id` == "SE627443-149526" ~ "NW627437-149509", # Brunnsjön
+        `EU id` == "SE653737-125017" ~ "NW653647-125036", # Ejgdesjön
+        `EU id` == "SE645289-128665" ~ "NW645343-128665", # Fräcksjön
+        `EU id` == "SE649314-149514" ~ "NW649253-149503", # Geten
+        `EU id` == "SE633209-141991" ~ "NW633246-141963", # Gyslättasjön
+        `EU id` == "SE643914-127698" ~ "NW643960-127717", # Härsvatten
+        `EU id` == "SE683582-154935" ~ "NW683582-154935", # Källsjön
+        `EU id` == "SE656640-164224" ~ "SE656590-164240", # Längsjön
+        `EU id` == "SE662682-132860" ~ "SE656590-164240", # Örvattnet
+        `EU id` == "SE674570-141911" ~ "NW674570-141911", # Rådsjön
+        `EU id` == "SE652902-125783" ~ "NW652888-125811", # Rotehogstjärnen
+        `EU id` == "SE666268-142230" ~ "NW666191-142252", # Skifsen
+        `EU id` == "SE656515-164330" ~ "SE656419-164404", # Stensjön
+        `EU id` == "SE664620-148590" ~ "NW664611-148550", # Västa Skälsjön
+        TRUE ~ `EU id`
+      ),
+      `MS_CD C3` = case_when(
+        `EU id` == "SE656612-164132" ~ "WA17355956", # Årsjön
+        `EU id` == "SE649314-149514" ~ "WA74940653", # Geten
+        `EU id` == "SE683582-154935" ~ "WA29887255", # Källsjön
+        `EU id` == "SE674570-141911" ~ "WA24280365", # Rådsjön
+        `EU id` == "SE666268-142230" ~ "WA70203019", # Skifsen
+        TRUE ~ `MS_CD C3`
+      ),
+      Stationsnamn = case_when(
+        Stationsnamn == "V. Skälsjön" ~ "Västra Skälsjön",
+        TRUE ~ Stationsnamn
+      )
+    ) -> joined_dataset
+  
+}
+
+
+#if(exists("Indexberakningar_data")==T){rm(Indexberakningar_data)}
+#if(exists("IKEU_indexberakningar")==T){rm(IKEU_indexberakningar)}
+if(exists("indexberakningar")==T){rm(indexberakningar)}
+#if(exists("Vaxtplankton_data")==T){rm(Vaxtplankton_data)}
+#if(exists("IKEU_Vaxtplankton")==T){rm(IKEU_Vaxtplankton)}
+if(exists("vaxtplankton")==T){rm(vaxtplankton)}
+#if(exists("Vattenkemi_data")==T){rm(Vattenkemi_data)}
+#if(exists("IKEU_Vattenkemi")==T){rm(IKEU_Vattenkemi)}
+if(exists("vattenkemi")==T){rm(vattenkemi)}
--- mergeVISSrivers.R
+++ mergeVISSrivers.R
@@ -1,21 +1,28 @@
 library(tidyverse)
 library(readxl)
+library(here)
 
-vdr<-read.csv("C:\\Users\\claudia\\Documents\\Projekt\\Havsmiljöinstitutet\\Sveriges Vattenmiljö 2018\\Sötvatten\\Koordinatfiler\\vattendrag med long lat.csv")%>%
-    mutate(Stationsnamn=case_when(Stationsnamn==as.character("Mattjåkkbäcken")~as.character("Mattjockbäcken"),
-                            TRUE ~as.character(Stationsnamn)))
+read.csv(here("Koordinatfiler/vattendrag med long lat.csv")) %>%
+    mutate(Stationsnamn = case_when(Stationsnamn == as.character("Mattjåkkbäcken") ~ as.character("Mattjockbäcken"),
+                                    TRUE ~as.character(Stationsnamn))) ->
+  vdr
 
-fisk<-read.csv("C:\\Users\\claudia\\Documents\\Projekt\\Havsmiljöinstitutet\\Sveriges Vattenmiljö 2018\\Sötvatten\\Koordinatfiler\\FiskVattendrag_lat_long.csv")
-fisk1<-distinct(fisk, MS_CD, .keep_all = TRUE)%>%
-  filter(EU_CD !="SE673902-577077")%>%  #Björnbackån
-  mutate(Vdragnamn=case_when(Vdragnamn==as.character("Mattjokkbäcken")~as.character("Mattjockbäcken"),
-                                TRUE ~as.character(Vdragnamn)))
+read.csv("Koordinatfiler/FiskVattendrag_lat_long.csv") %>%
+  distinct(MS_CD,
+           .keep_all = TRUE) %>%
+  filter(EU_CD != "SE673902-577077") %>%  #Björnbackån
+  mutate(Vdragnamn = case_when(Vdragnamn == as.character("Mattjokkbäcken") ~ as.character("Mattjockbäcken"),
+                               TRUE ~ as.character(Vdragnamn))) ->
+  fisk
 
-new_vdr<-full_join(vdr, fisk1, by=c("MS_CD.C3"="MS_CD", "Stationsnamn"="Vdragnamn")) %>% select(X.x,Y.x, X.y, Y.y, Stationsnamn, EU.id, MS_CD.C3, EU_CD)
+vdr %>%
+  full_join(fisk, by = c("MS_CD.C3" = "MS_CD", "Stationsnamn" = "Vdragnamn")) %>%
+  select(X.x, Y.x, X.y, Y.y, Stationsnamn, EU.id, MS_CD.C3, EU_CD) ->
+  new_vdr
 
 #olika<-full_join(vdr, fisk1, by=c("MS_CD.C3"="MS_CD", "EU.id"="EU_CD"))%>%
 #  select(EU.id, MS_CD.C3, Stationsnamn, Vdragnamn)
-#write.csv(olika, file = "C:\\Users\\claudia\\Documents\\Projekt\\Havsmiljöinstitutet\\Sveriges Vattenmiljö 2018\\Sötvatten\\Koordinatfiler\\olikastationer_sammavattendrag.csv",row.names=FALSE, na="")
+#write.csv(olika, file = "Koordinatfiler/olikastationer_sammavattendrag.csv",row.names=FALSE, na="")
 
 
 #MS_CD, MS_CD.C3
@@ -24,32 +31,46 @@ new_vdr<-full_join(vdr, fisk1, by=c("MS_CD.C3"="MS_CD", "Stationsnamn"="Vdragnam
 #          mutate(Stationsnamn=ifelse(is.na(Stationsnamn), as.character(NAMN), as.character(Stationsnamn)), Typ="Sjö", Vattenmiljö="Sjöar och Vattendrag", Region="") %>%
 #    select(Vattenförekomst, stationsID,Typ, Vattenmiljö, Region, long, lat)
 
-flodmynningar<-read_delim("stationer_flodmynningar.csv", 
-                          ",", escape_double = FALSE, locale = locale(encoding = "WINDOWS-1252"), 
-                          trim_ws = TRUE)%>%
-  select(StationsID, StnNamn)
+read_delim(here("Koordinatfiler/stationer_flodmynningar.csv"),
+           ",",
+           escape_double = FALSE,
+           locale = locale(encoding = "WINDOWS-1252"),
+           trim_ws = TRUE) %>%
+  select(StationsID, StnNamn) ->
+  flodmynningar
 
-Regioner<-read_delim("C:\\Users\\claudia\\Documents\\Projekt\\Havsmiljöinstitutet\\Sveriges Vattenmiljö 2018\\Sötvatten\\Koordinatfiler\\Vattendrag_Regioner.csv", 
-                     ",", escape_double = FALSE, locale = locale(encoding = "WINDOWS-1252"), 
-                     trim_ws = TRUE)
-  
-
-Koordinatfil<-new_vdr %>%   mutate(StationsID=case_when(is.na(`EU.id`)~as.character(EU_CD),
-                                                        TRUE~as.character(`EU.id`)),
-                                   long=case_when(is.na(X.x)~X.y,
-                                                  TRUE~X.x),
-                                   lat=case_when(is.na(Y.x)~Y.y,
-                                                 TRUE~Y.x),
-                                  VattenförekomstID=`MS_CD.C3`)%>%
-  left_join(flodmynningar)%>%
-  left_join(Regioner)%>%
-  mutate(Typ="Vattendrag",
-         Vattenmiljö=case_when(is.na(StnNamn)~"Sjöar och Vattendrag",
-                               StationsID=="SE646771-129330"~"Sjöar och Vattendrag",
-                       TRUE~"Flodmynningar")) %>%
-    select(Stationsnamn, StationsID, VattenförekomstID, Typ, Vattenmiljö, Region, long, lat) %>%arrange(Stationsnamn)
+read_delim(here("Koordinatfiler/Vattendrag_Regioner.csv"),
+                     ",", escape_double = FALSE, locale = locale(encoding = "WINDOWS-1252"),
+                     trim_ws = TRUE) ->
+  Regioner
 
 
+new_vdr %>%
+  mutate(StationsID = case_when(is.na(`EU.id`) ~ as.character(EU_CD),
+                                TRUE ~ as.character(`EU.id`)),
+         long = case_when(is.na(X.x) ~ X.y,
+                          TRUE ~ X.x),
+         lat = case_when(is.na(Y.x) ~ Y.y,
+                         TRUE ~ Y.x),
+         VattenförekomstID = `MS_CD.C3`) %>%
+  left_join(flodmynningar) %>%
+  left_join(Regioner) %>%
+  mutate(Typ = "Vattendrag",
+         Vattenmiljö = case_when(is.na(StnNamn) ~ "Sjöar och Vattendrag",
+                                 StationsID == "SE646771-129330" ~ "Sjöar och Vattendrag",
+                                 TRUE ~ "Flodmynningar")) %>%
+  select(Stationsnamn,
+         StationsID,
+         VattenförekomstID,
+         Typ,
+         Vattenmiljö,
+         Region,
+         long,
+         lat) %>%
+  arrange(Stationsnamn) ->
+  Koordinatfil
 
-write.csv(Koordinatfil, file = "C:\\Users\\claudia\\Documents\\Projekt\\Havsmiljöinstitutet\\Sveriges Vattenmiljö 2018\\Sötvatten\\Koordinatfiler\\RiverHierarkies_R.csv",row.names=FALSE, na="")
+
+
+write.csv(Koordinatfil, file = here("Koordinatfiler/RiverHierarkies_R.csv"), row.names = FALSE, na = "")
 
